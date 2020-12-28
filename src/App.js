34d4e3db984a5fdf8db7a17f53ccf7deb7a32bd4
Anishsaah <anishsaah@gmail.com>
2020-12-29 01:45:55 +0545

    Updated todoapp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import TextField from'@material-ui/core/TextField';
-import { Button } from '@material-ui/core';
+import { Grid,Button,List,ListItem,ListItemAvatar,ListItemText,ListItemSecondaryAction,IconButton,Avatar } from '@material-ui/core';
+import BurstModeIcon from '@material-ui/icons/BurstMode';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 export default class App extends Component {
   render() {
     return (
       <div align="center">
-        <h1>To Do App</h1>
+        <h1>To Do App <text style={{color:'lightgray',fontSize:10}}>By Anish Don</text></h1>
         <form>
           <TextField
             id="outlined-textarea"
@@ -17,9 +19,32 @@ export default class App extends Component {
             variant="outlined"          
           />
           &nbsp;&nbsp;&nbsp;<Button variant="contained" size="large">Add Your Thing</Button>
+          <div style={{marginLeft:"36%"}}>
+            <Grid container>
+            <List>
+              <ListItem>
+                <ListItemAvatar>
+                  <Avatar>
+                    <BurstModeIcon />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                  primary="Things to do" secondary="Jan 1,2021"/>
+                <div style={{paddingLeft:50}}>
+                <ListItemSecondaryAction>
+                  <IconButton edge="end" aria-label="delete">
+                    <DeleteIcon />
+                  </IconButton>
+                </ListItemSecondaryAction>
+                </div>
+              </ListItem>
+              
+              </List>
+            </Grid>
+          </div>  
         </form>
-
       </div>
+
     )
   }
 }
